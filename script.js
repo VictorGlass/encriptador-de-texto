e5098781febc754110b03c8aca9b404fb119b9de
Victor Carrera <enginemaster551@gmail.com>
2023-05-30 17:48:13 -0400

    Diseño de la aplicacion con CSS, diseño responsivo, ademas agregando la funcionalidad de encriptar el texto

---
+++ script.js
@@ -0,0 +1,26 @@
+//Definiendo Funciones//
+function encriptar() {
+    var texto = document.getElementById("texto").value;
+    var tituloMensaje = document.getElementById("titulo-mensaje");
+    var parrafo = document.getElementById("parrafo");
+    var muñeco = document.getElementById("muñeco");
+
+    var textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");    
+        
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/encriptado.jpg";
+    } else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }    
+}
